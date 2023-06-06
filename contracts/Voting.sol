49f712cd10c9295a4f3b33845d390530238cebf0
Jean NEPAS <jean.nepas@yopmail.com>
2023-06-06 15:25:25 +0200

    projet2

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.13;
-import "@openzeppelin/contracts/access/Ownable.sol";
+pragma solidity ^0.8.13;
+import access/Ownable.sol";
 
 
 contract Voting is Ownable {
