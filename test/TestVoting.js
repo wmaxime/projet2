b0bbb8e02f5b8a3f052ad3dcbab61a506a6689dc
wmaxime <wmaxime@paris.com>
2023-06-10 11:57:13 +0200

    Recuperationa architecture test SimpleStorage

--- test/TestVoting.js
+++ test/TestVoting.js
@@ -1,3 +1,37 @@
 const Voting = artifacts.require("Voting");
 const { BN, expectRevert, expectEvent } = require('@openzeppelin/test-helpers');
 const { expect } = require('chai');
+
+contract('SimpleStorage', accounts => {
+    const owner = accounts[0];
+    const second = accounts[1];
+    const third = accounts[2];
+
+    let StorageInstance;
+
+    describe("test require and event", function () {
+        
+        context("test sur telle fonction", function () {
+
+            beforeEach(async function () {
+                StorageInstance = await SimpleStorage.new({from:owner});
+            });
+
+            it("should verify require pass", async () => {
+                await StorageInstance.set(8, { from: owner });
+                const storedData = await StorageInstance.get();
+                expect(storedData).to.be.bignumber.equal(BN(8));
+            });
+
+            it("should verify require not passing", async () => {
+                await expectRevert(StorageInstance.set(42, { from: owner }), 'pas bon'); 
+            });
+
+            it("should verify event", async () => {
+                const findEvent = await StorageInstance.set(8, { from: owner });
+                expectEvent(findEvent,"Setted" ,{_value: BN(8)});
+            });
+
+        });
+    });
+});
